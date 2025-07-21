ab483f86e9f842c7095ef8aabe13dc3e676d4b06
deveshshrestha20 <deveshshrestha20@gmail.com>
2025-07-21 17:01:59 +0545

    Basic Folder Structure Created

---
+++ backend/app/core/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Literal
+
+class Settings(BaseSettings):
+    ENVIRONMENT: Literal["Local","staging", "production"] = "Local"
+
+    model_config = SettingsConfigDict(
+        env_file = "../../.envs/.env.local",
+        env_ignore_empty=True,
+        extra = 'ignore'
+    )
+
+
+settings = Settings()
