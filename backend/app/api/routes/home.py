84c108f25834b9885f53b4c70a4c266892c4ecd2
deveshshrestha20 <deveshshrestha20@gmail.com>
2025-07-22 04:31:47 +0545

    Added Loguru for logging

--- backend/app/api/routes/home.py
+++ backend/app/api/routes/home.py
@@ -1,9 +1,16 @@
 from fastapi import APIRouter
+from backend.app.core.logging import get_logger
 
+logger = get_logger()
 router = APIRouter()
 
 @router.get("/")
 def home():
+    logger.info("Home Page Accessed")
+    logger.debug("Home Page Accessed")
+    logger.error("Home Page Accessed")
+    logger.warning("Home Page Accessed")
+    logger.critical("Home Page Accessed")
     return {"message": "Welcome to the Bank API !!!!"}
 
 
