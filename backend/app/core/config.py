fab00d01411ddc495c1190ba2804337e2b45cc56
deveshshrestha20 <deveshshrestha20@gmail.com>
2025-07-22 13:41:21 +0545

    Added Docker and Postgres docker container

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,10 +5,12 @@ class Settings(BaseSettings):
     ENVIRONMENT: Literal["Local","staging", "production"] = "Local"
 
     model_config = SettingsConfigDict(
-        env_file = "../../.envs/.env.local",
+        env_file = "backend/app/envs/.env.local",
         env_ignore_empty=True,
         extra = 'ignore'
     )
+
+    DATABASE_URL: str = ""
 
 
 settings = Settings()
--- backend/app/core/logging.py
+++ backend/app/core/logging.py
@@ -8,15 +8,24 @@ logger.remove()
 LOG_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), "logs")
 
 LOG_FORMAT = (
-    "{time:YYYY-MM-DD HH:mm:ss.SSS} | {level} | {message}"
-    
-    "{level: <8} | "
-
-    "{name} : {function} : {line} - "
-    "{message}"
+    "{time:YYYY-MM-DD HH:mm:ss.SSS} | {level: <8} | "
+    "{name}:{function}:{line} - {message}"
 )
 
-logger.add(sink=os.path.join(LOG_DIR,"debug.log"), format=LOG_FORMAT, level="DEBUG " if settings.ENVIRONMENT == "local" else "INFO", filter = lambda record: record['level'].no <= logger.level("WARNING").no, rotation="10 MB", retention="30 days", compression="zip",backtrace=True, diagnose=True)
+
+logger.add(sink=os.path.join(LOG_DIR,"debug.log"), format=LOG_FORMAT, level="DEBUG" if settings.ENVIRONMENT == "local" else "INFO", filter = lambda record: record['level'].no <= logger.level("WARNING").no, rotation="10 MB", retention="30 days", compression="zip",backtrace=True, diagnose=True)
+
+# Log only ERROR and above to error.log
+logger.add(
+    sink=os.path.join(LOG_DIR, "error.log"),
+    format=LOG_FORMAT,
+    level="ERROR",  # Only log errors and critical
+    rotation="5 MB",
+    retention="60 days",
+    compression="zip",
+    backtrace=True,
+    diagnose=True
+)
 
 def get_logger():
     return logger
